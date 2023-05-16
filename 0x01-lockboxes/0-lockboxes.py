112f91823a5fdbc62e57ca8220c620ab453209a9
Biruk Tassew <89268568+Biruk-Tassew@users.noreply.github.com>
2023-05-16 23:35:57 +0300

    Update 0-lockboxes.py

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -11,10 +11,9 @@ def can_unlock_all(boxes):
     Check if boxes can be unlocked
     """
     for target_key in range(1, len(boxes) - 1):
-        is_unlocked = False
         for box_idx in range(len(boxes)):
             if target_key in boxes[box_idx] and target_key != box_idx:
                 break
-        if not is_unlocked:
-            return is_unlocked
+        if not target_key in boxes[box_idx] and target_key != box_idx:
+            return target_key in boxes[box_idx] and target_key != box_idx
     return True
