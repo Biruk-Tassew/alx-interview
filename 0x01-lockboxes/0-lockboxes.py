dc4d17832747d66c3c1b9dbb00624fb59a97bbc2
Biruk Tassew <89268568+Biruk-Tassew@users.noreply.github.com>
2023-05-16 23:32:51 +0300

    Update 0-lockboxes.py

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -6,16 +6,17 @@ Using prototype: def can_unlock_all(boxes)
 """
 
 
-def canUnlockAll(boxes):
+def can_unlock_all(boxes):
     """
     Check if boxes can be unlocked
     """
     for target_key in range(1, len(boxes) - 1):
-        is_unlocked = False
-        for box_idx in range(len(boxes)):
-            is_unlocked = target_key in boxes[box_idx] and target_key!=box_idx
-            if is_unlocked:
-                break
-        if not is_unlocked:
-            return is_unlocked
+        is_unlocked = (
+            target_key in boxes[box_idx] and
+            target_key != box_idx
+        )
+        if is_unlocked:
+            break
+    else:
+        return False
     return True
