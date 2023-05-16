0a433941280bb0846667e4ce63fb054ffb890838
Biruk Tassew <89268568+Biruk-Tassew@users.noreply.github.com>
2023-05-16 21:12:19 +0300

    Create 0-pascal_triangle.py

---
+++ 0-pascal_triangle.py
@@ -0,0 +1,16 @@
+def print_triangle(triangle):
+    """
+    Print the triangle
+    """
+    
+    dp = [[1]]
+        
+    for i in range(1, numRows):
+        new_row = [1]
+        for j in range(1, i):
+            new_row.append(dp[i-1][j]+dp[i-1][j-1])
+            
+        new_row.append(1)
+        dp.append(new_row)
+        
+    return dp
