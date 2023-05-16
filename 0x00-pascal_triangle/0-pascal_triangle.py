72421b158aae1374fae2ee5505c54e9230cb9052
Biruk Tassew <89268568+Biruk-Tassew@users.noreply.github.com>
2023-05-16 23:18:00 +0300

    Update 0-pascal_triangle.py

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+
 """
 0-pascal_triangle
 """
@@ -11,11 +12,13 @@ def pascal_triangle(triangle):
     dp = []
     if triangle <= 0:
         return dp
+
     dp = [[1]]
     for i in range(1, triangle):
         new_row = [1]
         for j in range(1, i):
-            new_row.append(dp[i-1][j]+dp[i-1][j-1])    
+            new_row.append(dp[i - 1][j] + dp[i - 1][j - 1])
         new_row.append(1)
         dp.append(new_row)
+
     return dp
