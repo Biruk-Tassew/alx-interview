d5cd4eb1ac8aa02d206f53f94066a93c1dead1e0
Biruk Tassew <89268568+Biruk-Tassew@users.noreply.github.com>
2023-05-17 23:57:18 +0300

    Update 0-stats.py

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,22 +1,47 @@
 #!/usr/bin/python3
-
-import random
+"""
+Script that reads stdin line by line and computes metrics
+"""
 import sys
-from time import sleep
-import datetime
+
+status_codes = {"200": 0, "301": 0, "400": 0, "401": 0, "403": 0, "404": 0, "405": 0, "500": 0}
+total_size = 0
 
 
-for i in range(1000):
-    sleep(random.random())
-    sys.stdout.write(
-        "{:d}.{:d}.{:d}.{:d} - [{}] \"GET /projects/260 HTTP/1.1\" {} {}\n".format(
-            random.randint(1, 255),
-            random.randint(1, 255),
-            random.randint(1, 255),
-            random.randint(1, 255),
-            datetime.datetime.now(),
-            random.choice([200, 301, 400, 401, 403, 404, 405, 500]),
-            random.randint(1, 1024)
-        )
-    )
-    sys.stdout.flush()
+def print_stats():
+    """
+    Function that prints stats about log
+    """
+    global total_size
+
+    print('File size: {}'.format(total_size))
+    sorted_codes = sorted(status_codes.keys())
+    for code in sorted_codes:
+        if status_codes[code] > 0:
+            print('{}: {}'.format(code, status_codes[code]))
+
+
+if __name__ == "__main__":
+    count = 0
+    try:
+        """ Iterate over the standard input """
+        for data in sys.stdin:
+            try:
+                parts = data.split(' ')
+                """ If there is a status code """
+                if parts[-2] in status_codes:
+                    status_codes[parts[-2]] += 1
+                """ If there is a length """
+                total_size += int(parts[-1])
+            except:
+                pass
+            """ Printing control """
+            count += 1
+            if count == 10:
+                print_stats()
+                count = 0
+    except KeyboardInterrupt:
+        print_stats()
+        raise
+    else:
+        print_stats()
